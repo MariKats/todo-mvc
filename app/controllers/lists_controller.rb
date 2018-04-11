b000dfee38a8cbf6936d3fc4fa7c7e36f09dba3f
github username <github email address>
2018-04-11 17:24:02 +0300

    first commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,23 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def create
+    list = List.create(list_params)
+    if list.save
+      redirect_to list_path(list)
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
