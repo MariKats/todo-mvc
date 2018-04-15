96edb5c6bde3dc52db2c2e34882309e38a2f8f4d
github username <github email address>
2018-04-15 19:03:05 +0300

    forms, some styling, formating

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -3,3 +3,13 @@ $(function(){
     $(this).parents("form").trigger("submit")
   })
 });
+
+$(document).ready(function() {
+  $('input.submit_on_enter').keydown(function(event) {
+    // enter has keyCode = 13, change it if you want to use another button
+    if (event.keyCode == 13) {
+      this.form.submit();
+      return false;
+    }
+  });
+});
